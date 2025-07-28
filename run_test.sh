8f947f8fc7b11f9fb01a6750870885bc0724d0cf
Aleksey Ermolaev <ermol-2012@yandex.ru>
2025-07-28 15:37:50 +0300

    add test for polyploid calling

--- run_test.sh
+++ run_test.sh
@@ -9,13 +9,18 @@ name="test"
 read1=$(readlink -f test_data/SRR33243472_1.fastq.gz)
 read2=$(readlink -f test_data/SRR33243472_2.fastq.gz)
 echo -e "${name}\t${read1}\t${read2}" > test_data/samples.tsv
-# for i in `seq 1 10`
-# do
-# echo -e "${i}\t${read1}\t${read2}" >> samples.tsv
-# done
 
 ./ChoCallate.py \
     --samples_tsv test_data/samples.tsv \
     --reference_index test_data/arth_chr1.fasta \
     --outdir test_data/chocallate_test \
     --reference_genome test_data/arth_chr1.fasta
+
+rm -rf test_data/chocallate_test
+
+./ChoCallate.py \
+    --samples_tsv test_data/samples.tsv \
+    --reference_index test_data/arth_chr1.fasta \
+    --outdir test_data/chocallate_test \
+    --reference_genome test_data/arth_chr1.fasta \
+    --ploidy 4
