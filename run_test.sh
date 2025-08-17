ddc746abb73392b0ebcb05b31b3db9b8e3a27386
Aleksey Ermolaev <ermol-2012@yandex.ru>
2025-08-17 16:21:00 +0300

    Update test data

--- run_test.sh
+++ run_test.sh
@@ -2,24 +2,18 @@
 set -euo pipefail
 
 echo "Prepearing genome index..."
-bowtie2-build --quiet --threads 4 test_data/arth_chr1.fasta test_data/arth_chr1.fasta
+bowtie2-build --quiet --threads 4 test_data/arth_chr1.fasta.gz test_data/arth_chr1.fasta.gz
 echo "Done"
 
 name="sample1"
-read1=$(readlink -f test_data/SRR33243472_1.fastq.gz)
-read2=$(readlink -f test_data/SRR33243472_2.fastq.gz)
-echo -e "${name}\t${read1}\t${read2}\t${read1}" > test_data/samples.tsv
-for i in 'sample2' 'sample3'; do echo -e "${i}\t${read1}\t${read2}\t${read1}" >> test_data/samples.tsv; done
+read1=$(readlink -f test_data/test_reads_R1.fq.gz)
+read2=$(readlink -f test_data/test_reads_R2.fq.gz)
+read3=$(readlink -f test_data/test_reads_SE.fq.gz)
+echo -e "${name}\t${read1}\t${read2}\t${read3}" > test_data/samples.tsv
 
-./ChoCallate.py \
+nextflow run main.nf \
     --samples_tsv test_data/samples.tsv \
-    --reference_index test_data/arth_chr1.fasta \
+    --reference_index test_data/arth_chr1.fasta.gz \
     --outdir test_data/chocallate_test \
-    --reference_genome test_data/arth_chr1.fasta
+    --reference_genome test_data/arth_chr1.fasta.gz
 
-./ChoCallate.py \
-    --samples_tsv test_data/samples.tsv \
-    --reference_index test_data/arth_chr1.fasta \
-    --outdir test_data/chocallate_test \
-    --reference_genome test_data/arth_chr1.fasta \
-    --ploidy 4
