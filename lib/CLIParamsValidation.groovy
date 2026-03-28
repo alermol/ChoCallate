62c03b33f035850e22179f114ee78c713588c9ff
Aleksey Ermolaev <ermol-2012@yandex.ru>
2026-03-28 17:48:19 +0300

    Update list of available calliers for validation and in config template

--- lib/CLIParamsValidation.groovy
+++ lib/CLIParamsValidation.groovy
@@ -14,7 +14,7 @@ class CLIParamsValidation {
     }
 
     static void effective_callers_validation(String callers, Number ploidy) {
-        def diploid_callers = ['bcftools', 'gatk', 'freebayes', 'snver', 'vardict']
+        def diploid_callers = ['bcftools', 'gatk', 'freebayes', 'snver', 'vardict', 'mutect2', 'varscan']
         def polyploid_callers = ['gatk', 'freebayes', 'snver']
         def callersList = callers.split(',')
         if (callers == null) {
